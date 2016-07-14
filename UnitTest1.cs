6efbb0258945ec019442f702d0cb999c79891579
StefanoCavalliBreton <cavalli.stefano@breton.it>
2016-07-14 12:13:37 +0200

    First Commit

---
+++ UnitTest1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void test1()
+        {
+            List<int> actual = PrimeFactor.generate(1);
+            List<int> expected = new List<int>();
+            Assert.IsTrue(expected.SequenceEqual(actual));
+        }
+    }
+
+    public class PrimeFactor
+    {
+        public static List<int> generate(int n)
+        {
+            return new List<int>();
+        }
+    }
+}
